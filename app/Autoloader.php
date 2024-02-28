43f54aa340096159c4bdab0a5be6347ec86b38ee
ThomasFortrie <thomas.fortrie@hotmail.fr>
2024-02-28 16:00:42 +0100

    first commit

---
+++ app/Autoloader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+
+
+class Autoloader
+{
+    static function register()
+    {
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+
+    static function autoload($className)
+    {
+        if(strpos($className, __NAMESPACE__ . '\\') ===0){
+            $class = str_replace(__NAMESPACE__ . '\\', '', $className);
+            $class = str_replace('\\', '/', $class);
+            require __DIR__ . '/' . $class . '.php';
+        }
+    }
+}
